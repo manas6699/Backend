c88e1a52d5f2ae14f8045d7704ca87ac44a469b8
manas6699 <manasbaroi6699@gmail.com>
2024-06-05 00:21:58 +0530

    type : module

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const express = require('express');
+// const express = require('express');
+import express from 'express'
+import {} from 'dotenv/config'
 const app = express();
 const port = process.env.PORT;
 
 app.get('/' , (req , res) => {
-    res.send('Hello')
-})
+    res.send('Hello');
+});
 
 app.listen(port , () => {
-    console.log(`App is running on port : ${port}`)
-})
+    console.log(`App is running on port : ${port}`);
+});
